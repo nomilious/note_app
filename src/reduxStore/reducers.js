97020639f82c4cd0b5a837e2e428fecbcc2ba2f1
nomilious <eduard.belic.2002@gmail.com>
2023-10-25 00:02:22 +0300

    No changes, just minor changes in file displaying

--- src/reduxStore/reducers.js
+++ src/reduxStore/reducers.js
@@ -10,14 +10,6 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.UPDATE_NOTE:
-            // Handle update note logic
-            return /* updated state */;
-        case actionTypes.SELECT_NOTE:
-            return { ...state, selectedNote: action.payload };
-        case actionTypes.CREATE_NOTE:
-            // Handle create note logic
-            return /* updated state */;
         case actionTypes.LOAD_DATA:
             return { ...state, data: action.payload };
         case actionTypes.UPDATE_NOTE_TITLE:
@@ -50,11 +42,16 @@ const rootReducer = (state = initialState, action) => {
             return {...state, data: updatedDataD}
         case actionTypes.TOGGLE_LOADING:
             return { ...state, loading: action.payload };
+        case actionTypes.SELECT_NOTE:
+            return { ...state, selectedNote: action.payload };
+        case actionTypes.SET_ERROR:
+            return { ...state, loading: action.payload };
         case actionTypes.DELETE_NOTE:
             // Handle delete note logic
             return /* updated state */;
-        case actionTypes.SET_ERROR:
-            return { ...state, loading: action.payload };
+        case actionTypes.CREATE_NOTE:
+            // Handle create note logic
+            return /* updated state */;
         default:
             return state;
     }

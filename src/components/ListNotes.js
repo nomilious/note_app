b93bc66ac6f0706f4890e3728487333fe8cc3caf
nomilious <eduard.belic.2002@gmail.com>
2023-10-24 18:15:21 +0300

    Made ListNotes.js scrollable

--- src/components/ListNotes.js
+++ src/components/ListNotes.js
@@ -1,55 +1,52 @@
-import { theme, ConfigProvider} from 'antd'; // Import Spin component
-import {  List,  } from "antd";
 import React from 'react';
-import { Typography  } from "antd";
+import { ConfigProvider, List, Typography, theme } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedNote } from '../reduxStore/actions';
+import './ListNotes.css'; // Import the CSS file for styling
 
+// TODO ensure that in the data.txt there is no empty lines
 function ListNotes() {
     const dispatch = useDispatch();
-    const data = useSelector(state => state.data)
+    const data = useSelector((state) => state.data);
     const selectedNote = useSelector((state) => state.selectedNote);
 
     const handleNoteClick = (index) => {
-        dispatch(setSelectedNote(index))
-    }
+        dispatch(setSelectedNote(index));
+    };
 
     return (
-        // TODO move ConfigProvider higher
-        // TODO add scrollbars https://ant.design/components/list
-        <ConfigProvider
-            theme={{
-                algorithm: [theme.defaultAlgorithm, theme.compactAlgorithm],
-            }}
-        >
-            <List
-                bordered
-                header={<div>Заметки</div>}
-                dataSource={data}
-                renderItem={(item, index) =>
-                {
-                    const isSelected = index === selectedNote;
-                    return (
-                        <List.Item
-                            key={index}
-                            onClick={() => handleNoteClick(index)}
-                            style={{ cursor: 'pointer', backgroundColor: isSelected ? '#f0f0f0' : 'white', }}>
-                            <List.Item.Meta title = {
-                                <Typography.Paragraph ellipsis={true}>
-                                    {item.title}
-                                </Typography.Paragraph>
-                            } description={
-                                <Typography.Paragraph ellipsis={true}>
-                                    {item.description || 'Нет тела'}
-                                </Typography.Paragraph>
-                            }/>
-                        </List.Item>
-                    );
-                }}
-            />
+        <ConfigProvider theme={{ algorithm: [theme.defaultAlgorithm, theme.compactAlgorithm], }}>
+            <div className="list-container">
+                <List
+                    bordered
+                    header={<div>Заметки</div>}
+                    dataSource={data}
+                    renderItem={(item, index) => {
+                        const isSelected = index === selectedNote;
+                        return (
+                            <List.Item
+                                key={index}
+                                onClick={() => handleNoteClick(index)}
+                                style={{
+                                    cursor: 'pointer',
+                                    backgroundColor: isSelected ? '#f0f0f0' : 'white',
+                                }}
+                            >
+                                <List.Item.Meta
+                                    title={<Typography.Paragraph ellipsis>{item.title}</Typography.Paragraph>}
+                                    description={
+                                        <Typography.Paragraph ellipsis>
+                                            {item.description || 'Нет тела'}
+                                        </Typography.Paragraph>
+                                    }
+                                />
+                            </List.Item>
+                        );
+                    }}
+                />
+            </div>
         </ConfigProvider>
     );
 }
-
 
 export default ListNotes;
